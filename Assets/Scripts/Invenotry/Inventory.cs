0fd9152755f2826e4b20c8484eb17a671a85379f
Luk-ass <lucasrzllzz0@gmail.com>
2025-03-20 23:19:43 +0100

    inventario comentado
    dani fachero

--- Assets/Scripts/Invenotry/Inventory.cs
+++ Assets/Scripts/Invenotry/Inventory.cs
@@ -6,16 +6,16 @@ using UnityEngine;
 public class Inventory : MonoBehaviour
 {
     public bool inventorytrue;
-    public GameObject inventory;
-    private int allslots;
-    private int enabledslots;
-    private GameObject[] slot;
-    public GameObject slotholder;
+    public GameObject inventory;//referncia del inventario
+    private int allslots;//referncia al numero de slots
+    private int enabledslots;//que slots estan disponibles
+    private GameObject[] slot;//la lista de los slots para refernciarlos
+    public GameObject slotholder;//referncia al contenedor de slots
     void Start()
     {
-        allslots = slotholder.transform.childCount;
+        allslots = slotholder.transform.childCount;//determina como slot todo lo que este dentro del objeto referenciado como slotholder
         slot = new GameObject[allslots];
-        for (int i = 0; i < allslots; i++)
+        for (int i = 0; i < allslots; i++)//mete en el array "slot" todos los slots creados
         {
             slot[i] = slotholder.transform.GetChild(i).gameObject;
             if (slot[i].GetComponent<Slots>().item == null)
@@ -27,7 +27,7 @@ public class Inventory : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.I))//El comando para abrir el inventario
         {
             inventorytrue = !inventorytrue;
         }
@@ -40,16 +40,16 @@ public class Inventory : MonoBehaviour
             inventory.SetActive(false);
         }
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)//recoge el objeto si el persnaje choca con su collider si tiene la tag Item
     {
         if (other.tag == "Item")
         {
             GameObject itempicked = other.gameObject;
             Item item = itempicked.GetComponent<Item>();
-            Additem(itempicked, item.ID, item.type, item.description, item.icon);
+            Additem(itempicked, item.ID, item.type, item.description, item.icon);// El objeto con la tag item debe tener el script item con los diferentes apartados rellenados en el inspector
         }
     }
-    public void Additem(GameObject itemobject, int itemID, string itemtype, string itemdescription, Sprite itemicon)
+    public void Additem(GameObject itemobject, int itemID, string itemtype, string itemdescription, Sprite itemicon)//actualiza el slot con la infoemacion del item recogido
     {
         for (int i = 0; i < allslots; i++)
         {
