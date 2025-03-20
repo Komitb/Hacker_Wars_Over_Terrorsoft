647865e5fcb034c854c98b9dbd69a3d708f72d5e
dankie94 <cpa.a4336@cpasalduie.com>
2025-03-20 21:02:51 +0100

    miniFinal
    paso despues de mi pedazo cagada

--- Assets/Scripts/Invenotry/Inventory.cs
+++ Assets/Scripts/Invenotry/Inventory.cs
@@ -27,7 +27,7 @@ public class Inventory : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.I))
         {
             inventorytrue = !inventorytrue;
         }
