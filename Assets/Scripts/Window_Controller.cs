454aa9325b86093f2562d5e2c98071b5839e7264
Komitb <iker.rubar27@gmail.com>
2025-04-01 16:46:08 +0200

    La ventana funciona
    🥶

--- Assets/Scripts/Window_Controller.cs
+++ Assets/Scripts/Window_Controller.cs
@@ -7,12 +7,14 @@ public class Window_Controller : MonoBehaviour
 {
     public float velocidad = 5f; //Velocidad de Movimiento
     public float opacidad = 1f;
+    public int childLeft;
     public  Image ImageUI;
     public Image Suciedad;
     private bool isMousePressed = false; //Para mantener la condición de si el ratón está presionado
     public GameObject VentanaExpandida;
     public GameObject Ventana;
     public GameObject dirtyOver;
+    public GameObject ChildDirt;
     Vector3 lastMousePos;
 
     // Start is called before the first frame update
@@ -20,14 +22,23 @@ public class Window_Controller : MonoBehaviour
     {
         Image ImageUI = GetComponent<Image>();
         Image suciedad = GetComponent<Image>();
+        foreach (Transform child in VentanaExpandida.GetComponentsInChildren<Transform>())
+        {
+            childLeft++;
+        }
+        childLeft--; //referencia
     }
 
     // Update is called once per frame
     void Update()
     {
-        Arrastrar();       
+        Arrastrar();    
+        if(childLeft == 0)
+        {
+            StartCoroutine(QuitVentana());
+        }
     }
-    public void Arrastrar() //Hace la funcion de arrastar el raton cuando tienes presionado el boton click izquierdo
+    public void Arrastrar() // Hace la función de arrastrar el ratón cuando tienes presionado el botón izquierdo
     {
         // Mantener presionado el botón izquierdo del ratón
         if (Input.GetMouseButton(0)) // 0 es el botón izquierdo del ratón
@@ -43,18 +54,23 @@ public class Window_Controller : MonoBehaviour
         {
             isMousePressed = false;
         }
+
         // Arrastrar y disminuir la opacidad de dirtyOver
         if (isMousePressed && dirtyOver != null && opacidad >= 0.2f)
         {
             // Obtener el ImageUI de dirtyOver
             Image dirtyOverImage = dirtyOver.GetComponent<Image>();
+
             opacidad = dirtyOverImage.color.a;
             opacidad -= opacidad * Time.deltaTime;
+
             // Disminuir la opacidad
             dirtyOverImage.color = new Color(dirtyOverImage.color.r, dirtyOverImage.color.g, dirtyOverImage.color.b, opacidad);
-            if (opacidad <= 0.2f)
+
+            if (opacidad <= 0.2f && dirtyOver.activeSelf) // Solo se decrementa una vez cuando opacidad llega a 0.2f
             {
                 dirtyOver.SetActive(false);
+                childLeft--; // Decrementamos childLeft solo una vez
             }
         }
         else
@@ -62,7 +78,8 @@ public class Window_Controller : MonoBehaviour
             isMousePressed = false;
             opacidad = 1f;
         }
-        if (Input.GetKeyDown(KeyCode.Return))//cunado le des al enter se activara la ventana
+
+        if (Input.GetKeyDown(KeyCode.Return)) // Cuando le das al enter se activará la ventana
         {
             ventana();
         }
@@ -83,5 +100,12 @@ public class Window_Controller : MonoBehaviour
     {
         Ventana.SetActive(false);
         VentanaExpandida.SetActive(true);
+    }
+    public IEnumerator QuitVentana()
+    {
+        yield return new WaitForSeconds(1);
+        VentanaExpandida.SetActive(false);
+        Ventana.SetActive(true);
+        childLeft = 0;
     }
 }
